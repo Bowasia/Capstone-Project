62dd5bda2fecb3a2977a430012b70d60de6a2172
Kan Ketkratuk <63011164@kmitl.ac.th>
2023-02-25 21:17:01 +0700

    Any update

--- Motor/define_io.h
+++ Motor/define_io.h
@@ -1,10 +1,10 @@
 // SPIN
 const int MOTOR_PIN_SP = 3;
-const int MOTOR_PIN_SM = 5;
+const int MOTOR_PIN_SN = 5;
 const int MOTOR_PWM_PINS = 6;
 
 
 // // FEED
 const int MOTOR_PIN_FP = 7;
-const int MOTOR_PIN_FM = 8;
+const int MOTOR_PIN_FN = 8;
 const int MOTOR_PWM_PINF = 9;
--- Motor/main.cpp
+++ Motor/main.cpp
@@ -2,35 +2,69 @@
 
 #include "define_io.h"
 
-// speed set up
-// Set the PWM duty cycle to 50% (out of 255)
-int spin_speed = 128;
-// ------
+unsigned long tTime[3] = {0,0};
+int sec = 1000;
+// Set the PWM duty cycle (out of 255)
+void spinspeed(int spin_speed_feed, int spin_speed_spin){
+  // Set the direction of the motor
+  //Spin
+  digitalWrite(MOTOR_PIN_SP, HIGH);
+  digitalWrite(MOTOR_PIN_SN, LOW);
+  analogWrite(MOTOR_PWM_PINS, spin_speed_spin);
+  // Feed
+  digitalWrite(MOTOR_PIN_FP, HIGH);
+  digitalWrite(MOTOR_PIN_FN, LOW);
+  analogWrite(MOTOR_PWM_PINF, spin_speed_feed);
+}
 
 void setup() {
   // Set Motor Driver pins to output mode
   // SPIN
   pinMode(MOTOR_PIN_SP, OUTPUT);
-  pinMode(MOTOR_PIN_SM, OUTPUT);
+  pinMode(MOTOR_PIN_SN, OUTPUT);
   pinMode(MOTOR_PWM_PINS, OUTPUT);
+  // FEED
+  pinMode(MOTOR_PIN_FP, OUTPUT);
+  pinMode(MOTOR_PIN_FN, OUTPUT);
+  pinMode(MOTOR_PWM_PINF, OUTPUT);
 
 }
 
 void loop() {
-  // Set the direction of the motor
-  digitalWrite(MOTOR_PIN_SP, LOW);
-  digitalWrite(MOTOR_PIN_SM, LOW);
-  // Wait for 2 seconds
-  delay(2000);
 
-  // Set the direction of the motor
-  digitalWrite(MOTOR_PIN_SP, HIGH);
-  digitalWrite(MOTOR_PIN_SM, LOW);
-  analogWrite(MOTOR_PWM_PINS, spin_speed);
+  unsigned long current_time = millis(); // The start time in milliseconds
 
-  // Set the PWM duty cycle to 75% (out of 255)
-  analogWrite(MOTOR_PWM_PINS, 192);
+  // ====================== every 20 sec, change duty
+  if(current_time - tTime[0] < 20*sec){ // 20 sec
+    spinspeed(255,64); // 25%
+  }
 
-  // Wait for 2 seconds
-  delay(2000);
+  if((current_time - tTime[0] > 20*sec) && (current_time - tTime[0] < 40*sec)){
+    spinspeed(255,128); // 50%
+  }
+
+  if((current_time - tTime[0] >= 40*sec) && (current_time - tTime[0] < 60*sec)){
+    spinspeed(255,191); // 75%
+  }
+
+  if((current_time - tTime[0] >= 60*sec) && (current_time - tTime[0] < 80*sec)){
+    spinspeed(255,255); // 100%
+  }
+
+  if((current_time - tTime[0] >= 80*sec) && (current_time - tTime[0] < 100*sec)){
+    spinspeed(255,255); // 100%
+  }
+
+  if((current_time - tTime[0] >= 100*sec) && (current_time - tTime[0] < 120*sec)){
+    digitalWrite(MOTOR_PIN_SP, LOW);
+    digitalWrite(MOTOR_PIN_SN, LOW);
+    digitalWrite(MOTOR_PIN_FP, LOW);
+    digitalWrite(MOTOR_PIN_FN, LOW);
+  }
+  // ======================
+
+  if(current_time - tTime[0] >= 120*sec){
+    tTime[0]=current_time;
+  }
+  
 }
