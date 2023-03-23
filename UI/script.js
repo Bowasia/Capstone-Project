e9270a3bd20c9d3944fa0250bfeced77731ae297
Bowasia <leejingsawat@gmail.com>
2023-03-23 21:41:59 +0700

    range control and schedule and toggle button checked

--- UI/script.js
+++ UI/script.js
@@ -7,3 +7,22 @@ range_display.innerHTML = range_input.value;
 range_input.oninput = function(){
     range_display.innerHTML = this.value;
 }
+
+// Time schedule
+var set_time = 0;
+
+function showtime(){
+    if (set_time == 0){
+        set_time = 1;
+    } else {
+        set_time = 0;
+    }
+    console.log(set_time);
+    if (set_time == 1){
+        document.getElementById("time-card").classList.add("time-card-on");
+        document.getElementById("topic-schedule").style.color = "var(--black)";
+    } else{
+        document.getElementById("time-card").classList.remove("time-card-on");
+        document.getElementById("topic-schedule").style.color = "var(--orange)";
+    }
+} 
